fcd71d130f72a4309a31f8cf544d0974a28fe18d
Shun <s.tsuchiya.business@gmail.com>
2024-06-27 00:24:07 +0900

    add test for timezone_validator

--- src/validator/timezone_validator.rs
+++ src/validator/timezone_validator.rs
@@ -6,3 +6,50 @@ pub(super) fn validate_string_for_timezone(tz: &str) -> Result<Tz, ValidationErr
         |_| ValidationError::InvalidTimezone(tz.to_string())
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono_tz::Tz;
+
+    /// Test that a valid timezone string is valid
+    /// expected: Ok(Tz)
+    #[test]
+    fn test_validate_string_for_timezone_valid() {
+        // confirm that a valid timezone string passes the validator
+        let tz_str = "America/New_York";
+        let result: Result<Tz, ValidationError> = validate_string_for_timezone(tz_str);
+        assert!(result.is_ok());
+
+        // confirm that the result value is the same as the expected
+        let expected_tz: Tz = tz_str.parse().unwrap();
+        assert_eq!(result.unwrap(), expected_tz);
+    }
+
+    /// Test that an invalid timezone is invalid
+    /// expected: Err(ValidationError::InvalidTimezone)
+    #[test]
+    fn test_validate_string_for_timezone_invalid() {
+        let tz_str = "Invalid/Timezone";
+        let result: Result<Tz, ValidationError> = validate_string_for_timezone(tz_str);
+        assert!(result.is_err());
+    }
+
+    /// Test that an empty string is invalid
+    /// expected: Err(ValidationError::InvalidTimezone)
+    #[test]
+    fn test_validate_string_for_timezone_empty_string() {
+        let tz_str = "";
+        let result: Result<Tz, ValidationError> = validate_string_for_timezone(tz_str);
+        assert!(result.is_err());
+    }
+
+    /// Test that a partial string is invalid
+    /// expected: Err(ValidationError::InvalidTimezone)
+    #[test]
+    fn test_validate_string_for_timezone_partial_string() {
+        let tz_str = "America";
+        let result: Result<Tz, ValidationError> = validate_string_for_timezone(tz_str);
+        assert!(result.is_err());
+    }
+}

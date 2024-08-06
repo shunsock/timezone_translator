3d422c323935e620b96576ac3e19eb2a84c3a01d
Shun <s.tsuchiya.business@gmail.com>
2024-08-06 23:49:04 +0900

    add e2e test / fix for test
    # Conflicts:
    #	Cargo.toml

--- src/validator/validation_error.rs
+++ src/validator/validation_error.rs
@@ -1,13 +1,13 @@
 #[derive(thiserror::Error, Debug, PartialEq)]
 pub(crate) enum ValidationError {
-    #[error("Invalid time format found: {0} (expected: YYYY-MM-DD hh:mm:ss)")]
+    #[error("Validation Error: Invalid time format found. {0} (expected: YYYY-MM-DD hh:mm:ss)")]
     TimeFormat(String),
 
     #[error(
-        "Invalid timezone found: {0}. @see https://docs.rs/chrono-tz/latest/chrono_tz/enum.Tz.html"
+        "Validation Error: Invalid timezone found {0}. @see https://docs.rs/chrono-tz/latest/chrono_tz/enum.Tz.html"
     )]
     Timezone(String),
 
-    #[error("Invalid ambiguous time strategy found: {ambiguous_time_strategy} (expected: earliest, latest)")]
+    #[error("Validation Error: Invalid ambiguous time strategy found. {ambiguous_time_strategy} (expected: earliest, latest)")]
     AmbiguousTimeStrategy { ambiguous_time_strategy: String },
 }
